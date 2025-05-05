9488c6de7943fd8d52b1296d8d2acf523077f87f
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-05-05 16:41:41 +0530

    added proxy changes

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const compression = require('compression');
-const cors = require('cors');
+// const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // Configuration
@@ -15,7 +15,7 @@ const app = express();
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use(compression());
-app.use(cors());
+// app.use(cors());
 // Set CORS headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
